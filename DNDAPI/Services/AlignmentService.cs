3126d5c1bf82d24f2147b09191d878be39be4c4b
Mounthein <mounthein1998@gmail.com>
2024-04-10 23:48:00 +0200

    Serveis creats i preparats

---
+++ DNDAPI/Services/AlignmentService.cs
@@ -0,0 +1,40 @@
+﻿using DNDAPI.Models;
+using MongoDB.Driver;
+
+namespace DNDAPI.Services
+{
+    public class AlignmentService : IAlignmentService
+    {
+        private readonly IMongoCollection<Alignment> _alignment;
+        public AlignmentService(IDndStoreDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _alignment = database.GetCollection<Alignment>(settings.DndAlignmentCollectionName);
+        }
+        public Alignment Create(Alignment alignment)
+        {
+            _alignment.InsertOne(alignment);
+            return alignment;
+        }
+
+        public List<Alignment> Get()
+        {
+            return _alignment.Find(alignment => true).ToList();
+        }
+
+        public Alignment Get(string id)
+        {
+            return _alignment.Find(alignemt => alignemt.Id == id).FirstOrDefault();
+        }
+
+        public void Remove(string id)
+        {
+            _alignment.DeleteOne(alignment => alignment.Id == id);
+        }
+
+        public void Update(string id, Alignment alignment)
+        {
+            _alignment.ReplaceOne(alignment => alignment.Id == id, alignment);
+        }
+    }
+}
---
+++ DNDAPI/Services/IAlignmentService.cs
@@ -0,0 +1,13 @@
+﻿using DNDAPI.Models;
+
+namespace DNDAPI.Services
+{
+    public interface IAlignmentService
+    {
+        List<Alignment> Get();
+        Alignment Get(string id);
+        Alignment Create(Alignment alignment);
+        void Update(string id, Alignment alignment);
+        void Remove(string id);
+    }
+}
