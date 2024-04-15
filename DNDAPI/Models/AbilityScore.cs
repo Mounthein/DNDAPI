2b755603798fa2738dd1758500bd9689d0ec0d86
Mounthein <mounthein1998@gmail.com>
2024-04-15 17:12:17 +0200

    Objecte AbilityScore creat

---
+++ DNDAPI/Models/AbilityScore.cs
@@ -0,0 +1,28 @@
+﻿using DNDAPI.Models.Commons;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DNDAPI.Models
+{
+    public class AbilityScore
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("index")]
+        public string Index { get; set; } = string.Empty;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [BsonElement("full_name")]
+        public string FullName { get; set; } = string.Empty;
+
+        [BsonElement("desc")]
+        public string[]? Description { get; set; }
+
+        [BsonElement("skills")]
+        public From[]? Skills { get; set;}
+    }
+}
