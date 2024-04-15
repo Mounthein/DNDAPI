4c537402908168a2667dac2ef3991c53d8d6b71f
Mounthein <mounthein1998@gmail.com>
2024-04-15 16:33:32 +0200

    Objectes creats

---
+++ DNDAPI/Models/Commons/From.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace DNDAPI.Models.Commons
+{
+    [BsonNoId]
+    public class From
+    {
+        [BsonElement("index")]
+        public string Index { get; set; } = String.Empty;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = String.Empty;
+    }
+}
---
+++ DNDAPI/Models/Skill.cs
@@ -0,0 +1,25 @@
+﻿using DNDAPI.Models.Commons;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DNDAPI.Models
+{
+    public class Skill
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = String.Empty;
+
+        [BsonElement("index")]
+        public string Index { get; set; } = String.Empty;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = String.Empty;
+
+        [BsonElement("desc")]
+        public string[]? Desc { get; set; }
+
+        [BsonElement("ability_score")]
+        public From? AbilityScore { get; set; }
+    }
+}
