038b7a254cc446790f9e1d936ccfd16cf17c1826
Mounthein <mounthein1998@gmail.com>
2024-04-15 17:20:00 +0200

    AbilityScore Services creats

---
+++ DNDAPI/Services/AbilityScoreService.cs
@@ -0,0 +1,41 @@
+﻿using DNDAPI.Models;
+using MongoDB.Driver;
+
+namespace DNDAPI.Services
+{
+    public class AbilityScoreService : IAbilityScoreService
+    {
+        private readonly IMongoCollection<AbilityScore> _abilities;
+
+        public AbilityScoreService(IDndStoreDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _abilities = database.GetCollection<AbilityScore>(settings.DndAbilityScoreCollectionName);
+        }
+        public AbilityScore Create(AbilityScore score)
+        {
+            _abilities.InsertOne(score);
+            return score;
+        }
+
+        public List<AbilityScore> Get()
+        {
+            return _abilities.Find(ability => true).ToList();
+        }
+
+        public AbilityScore Get(string id)
+        {
+            return _abilities.Find(ability => ability.Id == id).FirstOrDefault();
+        }
+
+        public void Remove(string id)
+        {
+            _abilities.DeleteOne(ability => ability.Id == id);
+        }
+
+        public void Update(string id, AbilityScore score)
+        {
+            _abilities.ReplaceOne(ability => ability.Id == id, score);
+        }
+    }
+}
---
+++ DNDAPI/Services/IAbilityScoreService.cs
@@ -0,0 +1,13 @@
+﻿using DNDAPI.Models;
+
+namespace DNDAPI.Services
+{
+    public interface IAbilityScoreService
+    {
+        List<AbilityScore> Get();
+        AbilityScore Get(string id);
+        AbilityScore Create(AbilityScore score);
+        void Update(string id, AbilityScore score);
+        void Remove(string id);
+    }
+}
