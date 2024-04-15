d530bcb2e91fdc30819f23da5127f468d74aaefa
Mounthein <mounthein1998@gmail.com>
2024-04-15 18:27:47 +0200

    AbilityScore Scope Afegit

--- DNDAPI/Program.cs
+++ DNDAPI/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddScoped<IWeaponPropertyService, WeaponPropertyService>();
 builder.Services.AddScoped<IConditionService, ConditionService>();
 builder.Services.AddScoped<ILanguageService, LanguageService>();
 builder.Services.AddScoped<ISkillService, SkillService>();
+builder.Services.AddScoped<IAbilityScoreService, AbilityScoreService>();
 
 
 builder.Services.AddControllers();
