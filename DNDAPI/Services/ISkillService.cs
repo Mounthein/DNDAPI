79c582c91577864f1e730f527aa56574fe4cd0cf
Mounthein <mounthein1998@gmail.com>
2024-04-15 16:45:27 +0200

    Serveis de Skill finalitzats

---
+++ DNDAPI/Services/ISkillService.cs
@@ -0,0 +1,13 @@
+﻿using DNDAPI.Models;
+
+namespace DNDAPI.Services
+{
+    public interface ISkillService
+    {
+        List<Skill> Get();
+        Skill Get(string skillId);
+        Skill Create(Skill skill);
+        void Update(string id, Skill skill);
+        void Remove(string id);
+    }
+}
