962a61016e56570be2c287dd5dd861e545bc7a98
Mounthein <mounthein1998@gmail.com>
2024-04-18 18:34:09 +0200

    Serveis i Model Creat

---
+++ DNDAPI/Services/EquipmentCategoryService.cs
@@ -0,0 +1,41 @@
+﻿using DNDAPI.Models;
+using MongoDB.Driver;
+
+namespace DNDAPI.Services
+{
+    public class EquipmentCategoryService : IEquipmentCategoryService
+    {
+        private readonly IMongoCollection<EquipmentCategory> _equipmentCategories;
+
+        public EquipmentCategoryService(IDndStoreDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+
+        }
+        public EquipmentCategory Create(EquipmentCategory equipmentCategory)
+        {
+            _equipmentCategories.InsertOne(equipmentCategory);
+            return equipmentCategory;
+        }
+
+        public List<EquipmentCategory> Get()
+        {
+            return _equipmentCategories.Find(equi => true).ToList();
+        }
+
+        public EquipmentCategory Get(string id)
+        {
+            return _equipmentCategories.Find(equi => equi.Id == id).FirstOrDefault();
+        }
+
+        public void Remove(string id)
+        {
+            _equipmentCategories.DeleteOne(equi => equi.Id == id);
+        }
+
+        public void Update(string id, EquipmentCategory equipmentCategory)
+        {
+            _equipmentCategories.ReplaceOne(equi => equi.Id == id, equipmentCategory);
+        }
+    }
+}
