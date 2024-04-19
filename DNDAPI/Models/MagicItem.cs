da42c92187010627a09ac420a3a897a3dd1c5455
Mounthein <mounthein1998@gmail.com>
2024-04-19 16:49:52 +0200

    Model Fet

---
+++ DNDAPI/Models/MagicItem.cs
@@ -0,0 +1,35 @@
+﻿using DNDAPI.Models.Commons;
+using DNDAPI.Models.SubModels;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DNDAPI.Models
+{
+    public class MagicItem
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = String.Empty;
+
+        [BsonElement("desc")]
+        public string[]? Desc { get; set; }
+
+        [BsonElement("equipment_category")]
+        public From? EquipmentCategory { get; set; }
+
+        [BsonElement("index")]
+        public string Index { get; set; } = String.Empty;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = String.Empty;
+
+        [BsonElement("rarity")]
+        public RarityMagicItem? Rarity { get; set; }
+
+        [BsonElement("variant")]
+        public Boolean? Variant { get; set; }
+
+        [BsonElement("variants")]
+        public string[]? Variants { get; set;}
+    }
+}
