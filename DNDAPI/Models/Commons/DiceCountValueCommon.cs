cc5f66e0be2d8d2699d75e178e5b810f12545c10
Mounthein <mounthein1998@gmail.com>
2024-04-19 16:03:15 +0200

    Models i submodels de Level

---
+++ DNDAPI/Models/Commons/DiceCountValueCommon.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace DNDAPI.Models.Commons
+{
+    [BsonNoId]
+    public class DiceCountValueCommon
+    {
+        [BsonElement("dice_count")]
+        public int? DiceCount { get; set; }
+
+        [BsonElement("dice_values")]
+        public int? DiceValues { get; set; }
+    }
+}
