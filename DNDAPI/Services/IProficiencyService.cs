d2dce73607532a4536553dbe1af995df423c7d1b
Mounthein <mounthein1998@gmail.com>
2024-04-22 16:50:08 +0200

    Proficiency services creats

---
+++ DNDAPI/Services/IProficiencyService.cs
@@ -0,0 +1,13 @@
+﻿using DNDAPI.Models;
+
+namespace DNDAPI.Services
+{
+    public interface IProficiencyService
+    {
+        List<Proficiency> Get();
+        Proficiency Get(string  id);
+        Proficiency Create(Proficiency proficiency);
+        void Update(string id, Proficiency proficiency);
+        void Remove(string id);
+    }
+}
---
+++ DNDAPI/Services/ProficiencyService.cs
@@ -0,0 +1,41 @@
+﻿using DNDAPI.Models;
+using MongoDB.Driver;
+
+namespace DNDAPI.Services
+{
+    public class ProficiencyService : IProficiencyService
+    {
+        private readonly IMongoCollection<Proficiency> _profiles;
+
+        public ProficiencyService(IDndStoreDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _profiles = database.GetCollection<Proficiency>(settings.DndProficiencyCollectionName);
+        }
+        public Proficiency Create(Proficiency proficiency)
+        {
+            _profiles.InsertOne(proficiency);
+            return proficiency;
+        }
+
+        public List<Proficiency> Get()
+        {
+            return _profiles.Find(proficienty => true).ToList();
+        }
+
+        public Proficiency Get(string id)
+        {
+            return _profiles.Find(proficienty => proficienty.Id == id).FirstOrDefault();
+        }
+
+        public void Remove(string id)
+        {
+            _profiles.DeleteOne(proficiency => proficiency.Id == id);
+        }
+
+        public void Update(string id, Proficiency proficiency)
+        {
+            _profiles.ReplaceOne(proficienty => proficiency.Id == id, proficiency);
+        }
+    }
+}
