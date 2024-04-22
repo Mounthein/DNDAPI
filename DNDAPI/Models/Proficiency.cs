d6fd216d06ad230b103923e0b7cca045d7c7d669
Mounthein <mounthein1998@gmail.com>
2024-04-22 16:42:43 +0200

    Proficiency Model creat

---
+++ DNDAPI/Models/Proficiency.cs
@@ -0,0 +1,28 @@
+﻿using DNDAPI.Models.Commons;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DNDAPI.Models
+{
+    public class Proficiency
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("index")]
+        public string Index { get; set; } = String.Empty;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = String.Empty;
+
+        [BsonElement("classes")]
+        public From[]? Classes { get; set; }
+
+        [BsonElement("races")]
+        public From[]? Races { get; set; }
+
+        [BsonElement("reference")]
+        public From? Reference { get; set; }
+    }
+}
