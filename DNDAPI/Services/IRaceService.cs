ae26c9941e57f9ab8510b7b76a89877b4b78c2ed
Mounthein <mounthein1998@gmail.com>
2024-04-22 17:47:44 +0200

    Race serveis creats

---
+++ DNDAPI/Services/IRaceService.cs
@@ -0,0 +1,13 @@
+﻿using DNDAPI.Models;
+
+namespace DNDAPI.Services
+{
+    public interface IRaceService
+    {
+        List<Race> Get();
+        Race Get(string id);
+        Race Create(Race race);
+        void Update(string id,  Race race);
+        void Remove(string id);
+    }
+}
---
+++ DNDAPI/Services/RaceService.cs
@@ -0,0 +1,42 @@
+﻿using DNDAPI.Models;
+using MongoDB.Driver;
+
+namespace DNDAPI.Services
+{
+    public class RaceService : IRaceService
+    {
+        private readonly IMongoCollection<Race> _race;
+
+        public RaceService(DndStoreDatabaseSettings settings, MongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _race = database.GetCollection<Race>(settings.DndRaceCollectionName);
+        }
+
+        public Race Create(Race race)
+        {
+            _race.InsertOne(race);
+            return race;
+        }
+
+        public List<Race> Get()
+        {
+            return _race.Find(race => true).ToList();
+        }
+
+        public Race Get(string id)
+        {
+            return _race.Find(race =>  race.Id == id).FirstOrDefault();
+        }
+
+        public void Remove(string id)
+        {
+            _race.DeleteOne(race => race.Id == id);
+        }
+
+        public void Update(string id, Race race)
+        {
+            _race.ReplaceOne(race => race.Id == id, race);
+        }
+    }
+}
